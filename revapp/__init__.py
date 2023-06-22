b2e0e30e91d76c351099e4f839722532d51dffbe
Aymane Boukrouh <boukrouhaymane@gmail.com>
2023-06-22 01:36:58 +0100

    add cors

--- revapp/__init__.py
+++ revapp/__init__.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 # app
 app = FastAPI()
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # endpoints
 from revapp.endpoints import *
